9756b1790ad4968fddd3c42550af9658080f76cf
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-22 01:32:18 -0400

    able to create a working posts index route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,12 @@
 import React, {useState, useEffect} from 'react'
+import IndexPost from '../pages/PostPages/IndexPost'
+import {Routes, Route} from 'react-router-dom'
 
 const Main = () => {
 
     const [posts, setPosts] = useState([])
     const URL = process.env.REACT_APP_POSTS_BACKEND_URL
-
-    console.log(URL)
     
-
     //Making an api call to the POSTs backend URL
     const getPosts = async () =>{
         const response = await fetch(URL)
@@ -26,7 +25,11 @@ const Main = () => {
 
 
     return (
-        <div>Main</div>
+        <main>
+            <Routes>
+                <Route path='/api/posts' element={<IndexPost posts={posts}  />}/>
+            </Routes>
+        </main>
     )
 }
 
--- src/pages/PostPages/IndexPost.js
+++ src/pages/PostPages/IndexPost.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+const IndexPost = (props) => {
 
-const IndexPost = () => {
+    //loaded function
+    const loaded = () => {
+        return (
+            <div className="containerIndex">  
+                {props.posts.map((post)=>(
+                <div key={post._id} >
+                    <Link to={`/api/posts/${post._id}`}>
+                        <h2>{post.title}</h2>
+                    </Link>
+                        <p>{post.postDesc}</p>
+                </div>
+                ))}
+            </div>
+            )
+        }
 
-    return (
-        <div>IndexPost</div>
-    )
+    const loading = () => {
+        return <h1>Loading... </h1> 
+    }
+
+    return (props.posts ? loaded() : loading())
 }
 
 export default IndexPost
