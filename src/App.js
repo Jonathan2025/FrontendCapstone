e3698595cc6bae8a510709952d9a0810158ee438
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-22 00:36:15 -0400

    starting to create some file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1> This is the app </h1>
+      <Header />
+      <Main />
     </div>
   )
 }
