a82553aea27043779c5f2a493e005ad4d26ce783
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-23 15:47:04 -0400

    testing with home and login, home page successfully reroutes to login if not authenticated

--- src/utils/RequireAuth.js
+++ src/utils/RequireAuth.js
@@ -1,13 +1,22 @@
-import {Route, Redirect} from 'react-router-dom'
+import {Route, useNavigate, useLocation} from 'react-router-dom'
 
 // Here we created a privateroute component which will allow someone to 
 // Children will be the element that is passed --> which are the routes that are passed.
 // In simple terms - we are wrapping the component in requireAuth 
 const RequireAuth =({ children }:{children: Element}) =>{
-  console.log("You reached Require Auth")
+  const navigate = useNavigate()
+  const location = useLocation()
+  const authenticated = false // for now we jsut set authenticated to false 
 
-  return children 
+  // if the user isnt authenticated then return them to the login page
+  if (!authenticated) {
+    navigate('/api/login')
+    return null
   }
+  // otherwise return the component
+  return children
+}
+
 
 
 export default RequireAuth
