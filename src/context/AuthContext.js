9d1ab1cf917501ec62f716f5f37662fe3495d427
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-23 21:58:33 -0400

    logged in successfully to the login form and submitted

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -14,29 +14,45 @@ export const AuthProvider = ({children}) => {
 
     const URL = process.env.REACT_APP_API_TOKEN_URL
     
+    
+
     // lets build out a login function
-    let loginUser = async() =>{
-        let response = fetch(URL, {
+    let loginUser = async(event) =>{
+        event.preventDefault()
+        let response = await fetch(URL, {
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
             },
-            body:JSON.stringify({'username':null, 'password':null})
+            // from the form submitted we will get the username and password
+            body:JSON.stringify({
+                'username':event.target.username.value, 
+                'password':event.target.password.value
+            })
         })
+
+        // if the response has an ok status 
+        if (response.ok) {
+            let data = await response.json();
+            console.log('data', data);
+        } else {
+            console.log(response)
+            console.error('Login failed, its Possible that you dont have an account registered');
+        }
     }
 
 
 
     // then add this function to context data 
-    let contextData={
-        loginUser:loginUser
-    }
+    // let contextData={
+    //     loginUser:loginUser
+    // }
 
 
 
     return (
         // Authentication context is then provided to the component's children
-        <AuthContext.Provider value={{'name': 'Jonathan'}}>
+        <AuthContext.Provider value={{loginUser}}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/Authentication/LoginPage.js
+++ src/pages/Authentication/LoginPage.js
@@ -1,15 +1,24 @@
-
+import { useContext } from "react"
+import AuthContext from "../../context/AuthContext"
 
 const LoginPage = () => {
+  let { loginUser } = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+     {/* When the form is submitted we want to fire off the loginuser function */}
+    event.preventDefault();
+    loginUser(event)
+  };
+
   return (
     <div>
-        <form> 
-            <input type="text" name="username" placeholder="Username" />
-            <input type="password" name="password" placeholder="Password" />
-            <input type="submit" />
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default LoginPage
