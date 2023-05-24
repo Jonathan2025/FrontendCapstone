fa0392b59b877fd556c4108b88286486a00a14c4
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-05-23 21:21:03 -0400

    building out the login user function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,6 +7,33 @@ export default AuthContext
 
 // The AuthProvider is a wrapper component that provides authentication context to its child components
 export const AuthProvider = ({children}) => {
+
+    
+    let[authTokens, setAuthTokens] = useState(null)
+    let[user, setUser]=useState(null)
+
+    const URL = process.env.REACT_APP_API_TOKEN_URL
+    
+    // lets build out a login function
+    let loginUser = async() =>{
+        let response = fetch(URL, {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'username':null, 'password':null})
+        })
+    }
+
+
+
+    // then add this function to context data 
+    let contextData={
+        loginUser:loginUser
+    }
+
+
+
     return (
         // Authentication context is then provided to the component's children
         <AuthContext.Provider value={{'name': 'Jonathan'}}>
