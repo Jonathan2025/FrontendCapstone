8685ad75250a52ecb70c077b8c9b1ec2a4b543ed
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-02 10:16:18 -0400

    rearranged the file structure to make it more organized

--- src/pages/Authentication/LoginPage.js
+++ src/pages/Authentication/LoginPage.js
@@ -1,5 +1,7 @@
 import { useContext } from "react"
 import AuthContext from "../../context/AuthContext"
+import { Link } from 'react-router-dom'
+import "../../styling/CSS/LoginPage.css"
 
 const LoginPage = () => {
   let { loginUser } = useContext(AuthContext);
@@ -13,11 +15,10 @@ const LoginPage = () => {
   return (
     <div class="signInPage row">
       <div className="shadingOverImage"></div> 
-        <div className="centerSignIn col s12 m6 l3">
+        <div className="centerSignIn col s12 m6 l4">
           <form class="signInForm col s12 " onSubmit={handleSubmit}>
             <h3 class="signInHeader">Sign In</h3>
 
-            
             <div class="row">
               <div class="signInInputDiv input-field col s12">
                 <input type="text" name="username" placeholder="Username" />
@@ -29,8 +30,14 @@ const LoginPage = () => {
                 <input type="password" name="password" placeholder="Password" />
               </div>
             </div>
+
+            <div class="row linkContainer">
+              <div class="linktoregister">
+                <p>New to KickFlix? </p> <Link to="/api/register">Register Now</Link>
+              </div>
+            </div>
         
-            <button class="btn red waves-effect waves-light" type="submit" name="action">Sign In </button>
+            <button class="signInBtn btn red waves-effect waves-light btn-large" type="submit" name="action">Sign In </button>
       
           </form>
       </div>
