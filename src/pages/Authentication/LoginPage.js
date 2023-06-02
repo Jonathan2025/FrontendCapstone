8953705a85eaa2d461237985077f421977dcb15b
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-01 21:49:49 -0400

    styled the login page a bit

--- src/pages/Authentication/LoginPage.js
+++ src/pages/Authentication/LoginPage.js
@@ -11,13 +11,29 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="submit" />
-      </form>
-    </div>
+    <div class="signInPage row">
+      <div className="shadingOverImage"></div> 
+        <div className="centerSignIn col s12 m6 l3">
+          <form class="signInForm col s12 " onSubmit={handleSubmit}>
+            <h3 class="signInHeader">Sign In</h3>
+            
+            <div class="row">
+              <div class="signInInputDiv input-field col s12">
+                <input type="text" name="username" placeholder="Username" />
+              </div>
+            </div>
+        
+            <div class="row">
+              <div class="signInInputDiv input-field col s12">
+                <input type="password" name="password" placeholder="Password" />
+              </div>
+            </div>
+        
+            <button class="btn red waves-effect waves-light" type="submit" name="action">Sign In </button>
+      
+          </form>
+      </div>
+</div>
   )
 }
 
