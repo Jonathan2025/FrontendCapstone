c2df7176f40940802eb3de22cae523b86b818c6b
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-02 10:38:01 -0400

    added some styling to the register page

--- src/pages/Authentication/RegisterPage.js
+++ src/pages/Authentication/RegisterPage.js
@@ -34,54 +34,94 @@ const RegisterPage = (props) => {
     
 
           return (
-            <div>
-              <h1>Register Here!</h1>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  name="username"
-                  value={newForm.username}
-                  placeholder="Username"
-                  onChange={handleChange}
-                />
-                <input
-                  type="password"
-                  name="password"
-                  value={newForm.password}
-                  placeholder="Password"
-                  onChange={handleChange}
-                />
-                <input
-                  type="password"
-                  name="password2"
-                  value={newForm.password2}
-                  placeholder="Confirm Password"
-                  onChange={handleChange}
-                />
-                <input
-                  type="email"
-                  name="email"
-                  value={newForm.email}
-                  placeholder="Email"
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  name="first_name"
-                  value={newForm.first_name}
-                  placeholder="First Name"
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  name="last_name"
-                  value={newForm.last_name}
-                  placeholder="Last Name"
-                  onChange={handleChange}
-                />
-                <input type="submit" value="Register" />
-              </form>
+            <div className="registerPage row">
+              <div className="shadingOverImage">
+                <div className="centerRegister col 212 m6 l4">
+                  
+                  <form className="registerForm col s12" onSubmit={handleSubmit}>
+                  <h3 className='registerHeader'>Register Here!</h3>
+                    
+                    <div class="row">
+                      <div class="registerInputDiv input-field col s12">
+                      <input
+                          type="text"
+                          name="username"
+                          value={newForm.username}
+                          placeholder="Username"
+                          onChange={handleChange}
+                        />
+                      </div> 
+                    </div>
+
+                    <div class="row">
+                      <div class="registerInputDiv input-field col s12">
+                        <input
+                          type="password"
+                          name="password"
+                          value={newForm.password}
+                          placeholder="Password"
+                          onChange={handleChange}
+                        />
+                      </div> 
+                    </div>
+
+                    <div class="row">
+                      <div class="registerInputDiv input-field col s12">
+                          <input
+                            type="password"
+                            name="password2"
+                            value={newForm.password2}
+                            placeholder="Confirm Password"
+                            onChange={handleChange}
+                          />
+                      </div> 
+                    </div>
+
+                    <div class="row">
+                      <div class="registerInputDiv input-field col s12">
+                          <input
+                            type="email"
+                            name="email"
+                            value={newForm.email}
+                            placeholder="Email"
+                            onChange={handleChange}
+                          />
+                      </div> 
+                    </div>
+
+                    <div class="row">
+                      <div class="registerInputDiv input-field col s12">
+                           <input
+                            type="text"
+                            name="first_name"
+                            value={newForm.first_name}
+                            placeholder="First Name"
+                            onChange={handleChange}
+                          />
+                      </div> 
+                    </div>
+
+                    <div class="row">
+                      <div class="registerInputDiv input-field col s12">
+                            <input
+                              type="text"
+                              name="last_name"
+                              value={newForm.last_name}
+                              placeholder="Last Name"
+                              onChange={handleChange}
+                            />
+                      </div> 
+                    </div>
+
+                    <button class="signInBtn btn red waves-effect waves-light btn-large" type="submit" name="action">Register </button>
+                  </form>
+                </div>
+             
+              </div>
             </div>
+
+            
+            
           )
         }
 
