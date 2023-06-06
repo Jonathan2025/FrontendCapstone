0de0642b2f55e076af794342e9e12da5210a877a
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-05 20:42:49 -0400

    edit form is able to show the originally selected values for category

--- src/pages/PostPages/EditPost.js
+++ src/pages/PostPages/EditPost.js
@@ -1,35 +1,47 @@
-import { useState } from "react"
-import { useParams, useNavigate, useLocation } from "react-router-dom"
+import { useState} from "react"
+import { useNavigate, useLocation } from "react-router-dom"
 import '../../styling/CSS/EditPost.css'
-
-
+import Multiselect from "multiselect-react-dropdown"
 
 const EditPost = (props) => {
     const location = useLocation()
     const post = location.state.post //in showPost.js we passed in the state:post so we can get the specific post being edited in less code
     const navigate = useNavigate()
 
-    const [editForm, setEditForm] = useState(post)
+    const [editForm, setEditForm] = useState({
+        ...post,
+        category: post.category.replace(/[\[\]']/g, '') //when we pass in the post.category data we get an array back so we want to remove unwanted characters with regex
+        .split(',') // split to get the items in the array back NOT the characters
+      })
+
+
+    const [category, setCategory] = useState(["Self Defense", "Basics", "Training", "Tutorial", "Beginner", "Intermediate","Advanced", "Jonathan's Originals", "Sparring", "Competition", "Comedy", "Blockbuster", "Thriller" ])
+
+    const handleCategorySelect = (selectedCategory) => {
+        setEditForm({ ...editForm, category: selectedCategory })
+    }
+      
+    const handleCategoryRemove = (removedCategory) => {
+        const updatedCategory = editForm.category.filter(
+        (category) => category !== removedCategory
+        )
+        setEditForm({ ...editForm, category: updatedCategory })
+    }
 
     const handleChange = (event) => {
         event.preventDefault()
-        // whatever gets changed, we change it to event.target.value
-        setEditForm({ ...editForm, [event.target.name]: event.target.value });
+        setEditForm({ ...editForm, [event.target.name]: event.target.value }) // whatever gets changed, we change it to event.target.value
     }
 
     const handleSubmit = (event) => {
         event.preventDefault() 
-        // updatePost takes 2 arguments: an object representing the edited form data and the id of the post being edited
-        props.updatePost(editForm, post.id)
-        // redirect people back to show page AFTER the user edits the information
-        navigate(`/api/posts/${post.id}`);
+        props.updatePost(editForm, post.id)  // updatePost takes 2 arguments: an object representing the edited form data and the id of the post being edited
+        navigate(`/api/posts/${post.id}`) // redirect people back to show page AFTER the user edits the information
     }
-
+    
     return (
         <div className="editPostPage row">
             <div className="centerEditPost col s12 m6 l4">
-                
-
                 <form className="editPostForm col s12 "onSubmit={handleSubmit}>
                     <h3 className="editPostHeader">Edit {post.title}?</h3>
                     
@@ -47,17 +59,19 @@ const EditPost = (props) => {
                     </div>
 
                     <div className="row">
-                    <div className="editPostInputDiv input-field col s12">
-                        <input
-                            type="text"
-                            value={editForm.category}
-                            name="category"
-                            placeholder="Post Category"
-                            onChange={handleChange}
-                            required
+                      <div className="createPostInputDiv">
+                      <Multiselect
+                          isObject={false}
+                          onRemove={handleCategoryRemove}
+                          onSelect={handleCategorySelect}
+                          options={category}
+                          onChange={handleChange}
+                          placeholder="Select a category"
+                        selectedValues={editForm.category}
                         />
                       </div>
                     </div>
+
 
                     <div className="row">
                     <div className="editPostInputDiv input-field col s12">
@@ -73,15 +87,10 @@ const EditPost = (props) => {
                       </div>
                     </div>
 
-                    {/* Hidden input field for editForm.upload */}
+                    {/* We decided to hide the input field for editForm.upload */}
                     <input type="hidden" name="upload" value={editForm.upload} />
-
-             
                     <button className="editPostBtn btn red waves-effect waves-light btn-large" type="submit" name="action">Edit Post </button>
                 </form>
-
-
-
             </div>
         </div>
     )
