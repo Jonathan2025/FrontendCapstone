33ef343ba47f6bc2334ef04ec71f515972224e8c
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-08 21:12:37 -0400

    able to customize the headers in index users

--- src/components/UserProfilesIndex/PictureSlider/PictureSlider.js
+++ src/components/UserProfilesIndex/PictureSlider/PictureSlider.js
@@ -4,11 +4,10 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-
 const PictureSlider = (props) => {
 
-    // Settings from React-Slick documentation, please refer to citations for more information
+    // Settings for the slider from React-Slick documentation, please refer to citations for more information
+    // Settings refer to how many items should show in various screen sizes
     const settings = {
         dots: true,
         infinite: false,
@@ -45,36 +44,53 @@ const PictureSlider = (props) => {
     }
 
 
-
-
-
-
-    //loaded function
     const loaded = () => {
+
+        // Now we pretty much want to create a separate slider for each martial art (from userProfiles)
+        const martialArtsSet = new Set() // first we create a set and add the martial arts from all present userProfiles
+        props.userProfiles.forEach((userProfile) => {
+            martialArtsSet.add(userProfile.martialArt)
+        })
+
+        const martialArts = Array.from(martialArtsSet) // convert the set to an array so its easier to work with 
+
         return (
-            // we will be using the Slider from react-slick to create the carousel. Pass in the settings from above
-            <Slider {...settings}> 
 
-                    {props.userProfiles.map((userProfile, index) => (
-                    <div key={index} className="card">
-                        <div className="card-top">
-                            <UserMediaContainer className="carousel-item" uploadFile={userProfile.picture} />
+            <>  
+                {/* From the list of PRESENT martial arts from the user profiles above, we want to create a slider for each of them */}
+                {martialArts.map((martialArt) => (
+                  <div>
+                    <h4 className="martialArtCategory">{martialArt}</h4>
+                    <Slider {...settings} key={martialArt}>
+                    
+                    {props.userProfiles
+                        .filter((userProfile) => userProfile.martialArt === martialArt)
+                        .map((userProfile, index) => (
+                        <div key={index} className="userCard">
+                            <Link to={`/api/userProfiles/${userProfile.id}`} className="userCard-link">
+                            <div className="userCard-top">
+                                <UserMediaContainer className="carousel-item" uploadFile={userProfile.picture} />
+                            </div>
+                            <div className="userCard-bottom">
+                                <h5 className="indexPageUserFullName">{userProfile.first_name + " " + userProfile.last_name}</h5>
+                                <h5>{userProfile.beltLevel}</h5>
+                            </div>
+                            </Link>
                         </div>
-                        <div className="card-bottom">
-                            <h5 className="indexPageUserFullName">{userProfile.first_name + " " + userProfile.last_name}</h5>
-                            <h5>{userProfile.beltLevel}</h5>
-                        </div>
+                        ))}
+                    </Slider>
                     </div>
-                        
-                 
-                    ))}
-
-            </Slider>
-
+                ))}
+                </>
 
         )
 
       }
+
+
+
+
+
 
     const loading = () => {
         return <h1>Loading... </h1> 
@@ -85,15 +101,10 @@ const PictureSlider = (props) => {
 
 export default PictureSlider
 
-         {/* {props.userProfiles.map((userProfile) => (
-              <div key={userProfile.id}>
-                <h2>{userProfile.martialArt}</h2>
-                <Link to={`/api/userProfiles/${userProfile.id}`}>
-                  <MediaContainer uploadFile={userProfile.picture}/>
-                </Link>
-                
-              </div>
-            ))} */}
+
+
+
+
 
 
 
--- src/pages/UserProfilePages/IndexUser.js
+++ src/pages/UserProfilePages/IndexUser.js
@@ -1,15 +1,12 @@
-import { Link } from 'react-router-dom'
 import InfiniteSlider from '../../components/UserProfilesIndex/InfiniteSlider/InfiniteSlider'
-import MediaContainer from '../../components/Media/PostMediaContainer'
 import PictureSlider from '../../components/UserProfilesIndex/PictureSlider/PictureSlider'
 const IndexUserProfile = (props) => {
 
     //loaded function
     const loaded = () => {
         return (
-
           <>
-          
+          <h3 className ="indexUserPageHeader">Meet Our Users</h3>
           <InfiniteSlider />
           <PictureSlider userProfiles={props.userProfiles} />
 
