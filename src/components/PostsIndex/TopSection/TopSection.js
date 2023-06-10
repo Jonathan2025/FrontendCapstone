53614c3efb5572f4893d090d551e8a101a486eff
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-10 13:41:48 -0400

    going to continue adding the video content overlay

--- src/components/PostsIndex/TopSection/TopSection.js
+++ src/components/PostsIndex/TopSection/TopSection.js
@@ -7,32 +7,37 @@ const TopSection = () => {
     
 
     <LazyLoad once className="topVideoContainer">
-       
-        <HoverVideoPlayer
-            className = "topVideoPreview"
-            videoSrc = {process.env.REACT_APP_TOP_SECTION_VIDEO}
-            style={{ width: '100%'}}
-            videoStyle={{ height: '650px', borderRadius: '20px'}}
-            muted={false}
-            pausedOverlay={
-                <img
-                className='topVideoImage'
-                src={TopSectionData[0].image}
-                alt="TopSectionImg"
-                style={{
-                    // Make the image expand to cover the video's dimensions
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover',
-                    borderRadius: '20px',
-
-                }}
-                />
-            }
-        />
-
+          <div className='content'> 
+            <h2>Hello there this is you captain speaking</h2>
+        </div>
+    <HoverVideoPlayer
       
-    </LazyLoad> 
+
+      className="topVideoPreview"
+      videoSrc={process.env.REACT_APP_TOP_SECTION_VIDEO}
+      style={{ width: '100%' }}
+      videoStyle={{ height: '650px', borderRadius: '20px', position: 'relative' }}
+      muted={false}
+      pausedOverlay={
+        <div className='pausedOverlay'>
+          <div className='content'>
+            <h2>Hello there, this is your captain speaking</h2>
+          </div>
+          <img
+            className='topVideoImage'
+            src={TopSectionData[0].image}
+            alt="TopSectionImg"
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              borderRadius: '20px',
+            }}
+          />
+        </div>
+      }
+    />
+  </LazyLoad>
   )
 }
 
