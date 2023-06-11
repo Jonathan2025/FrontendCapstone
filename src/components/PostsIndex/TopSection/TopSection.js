6023e00a14c5cfa6449da1353ac89d791516b17c
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-11 17:17:58 -0400

    created a top video section for the index page

--- src/components/PostsIndex/TopSection/TopSection.js
+++ src/components/PostsIndex/TopSection/TopSection.js
@@ -2,42 +2,58 @@ import HoverVideoPlayer from 'react-hover-video-player'
 import {TopSectionData} from "./TopSectionData"
 import LazyLoad from "react-lazyload"
 import "./TopSection.css"
+import { FaPlay } from 'react-icons/fa'
+import { FiInfo } from 'react-icons/fi'
+import warriorLogo from '../../../styling/Images/Cinemax_Warrior.webp'
+
 const TopSection = () => {
+
   return (
-    
+  <LazyLoad once className="topVideoContainer">
+    <div style={{ position: 'relative' }}>
+      <HoverVideoPlayer
+        className="topVideoPreview"
+        videoSrc={process.env.REACT_APP_TOP_SECTION_VIDEO}
+        style={{ width: '100%'}}
+        videoStyle={{ height:'70vh', borderRadius: '20px', position: 'relative' }}
+        muted={false}
 
-    <LazyLoad once className="topVideoContainer">
-          <div className='content'> 
-            <h2>Hello there this is you captain speaking</h2>
-        </div>
-    <HoverVideoPlayer
-      
-
-      className="topVideoPreview"
-      videoSrc={process.env.REACT_APP_TOP_SECTION_VIDEO}
-      style={{ width: '100%' }}
-      videoStyle={{ height: '650px', borderRadius: '20px', position: 'relative' }}
-      muted={false}
-      pausedOverlay={
-        <div className='pausedOverlay'>
-          <div className='content'>
-            <h2>Hello there, this is your captain speaking</h2>
+        // When video isnt playing, an image will be shown (pausedOverLay)
+        pausedOverlay={
+          <div className="pausedOverlay">
+            <img
+              className='topVideoImage'
+              src={TopSectionData[0].image}
+              alt="TopSectionImg"
+              style={{
+                width: '100%',
+                height: '100%',
+                borderRadius: '20px',
+                backgroundPosition: 'center',
+                position: 'absolute',
+                objectFit: 'cover',
+                display:'block',
+                top: 0,
+                left: 0,
+              }}
+            />
           </div>
-          <img
-            className='topVideoImage'
-            src={TopSectionData[0].image}
-            alt="TopSectionImg"
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              borderRadius: '20px',
-            }}
-          />
+        }
+        />
+        {/* Contents of the video */}
+          <div className="banner_contents">
+            <img src={warriorLogo} alt="Warrior Logo" className="banner_logo" />
+            <div className="banner_buttons"> 
+              <button className="banner_play_button">
+                <FaPlay className='playIcon'/> Play
+              </button>
+              <button className="banner_info_button">
+                <FiInfo className='infoIcon' /> Info
+              </button>
+            </div>
+          </div>
         </div>
-      }
-    />
-  </LazyLoad>
+      </LazyLoad>
   )
 }
 
--- src/components/PostsIndex/TopSection/TopSectionData.js
+++ src/components/PostsIndex/TopSection/TopSectionData.js
@@ -1,7 +1,7 @@
 // create the data for the slides we will be adding 
 export const TopSectionData = [
     {
-        image: "https://i.ytimg.com/vi/z7WRXO-Hxv4/maxresdefault.jpg",
+        image: "https://cdn.vox-cdn.com/thumbor/A8yHHJXgllrHxXjt7nU3rO6z90w=/0x0:1920x1280/1200x800/filters:focal(807x487:1113x793)/cdn.vox-cdn.com/uploads/chorus_image/image/68808627/chen_tang_jason_tobin_andrew_koji.0.jpg",
         heading: "Coming Soon! Warrior Season 3 | Official Trailer | Max",
         desc: "From HBO Max, comes the new season of Warrior. Ah Sahm, a martial arts prodigy who emigrates from China to San Francisco under mysterious circumstances and becomes a hatchet man for one of Chinatown's most powerful tongs ", 
         link: "https://www.youtube.com/watch?v=89dEEKEzglw"
