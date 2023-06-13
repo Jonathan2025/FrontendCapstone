fe6f45337c15c36d9afa0c38c51119ecd23f2287
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-06-12 22:40:50 -0400

    created a hamburger nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,3 @@
-
 import {useContext, useEffect} from 'react'
 import AuthContext from '../../context/AuthContext'
 import { Link } from 'react-router-dom'
@@ -8,19 +7,39 @@ import { MdLogout, MdCreate, MdOutlineAccountBox } from 'react-icons/md'
 
 const NavBar = () => {
     let {user, logoutUser} = useContext(AuthContext)
-    console.log(user.username)
+    
     useEffect(() => {
-        // Initialize dropdown
-        let dropdowns = document.querySelectorAll('.dropdown-trigger');
+        // Initialize dropdown menu for the account
+        let dropdowns = document.querySelectorAll('.dropdown-trigger')
           M.Dropdown.init(dropdowns, {
             coverTrigger: false,
             container: document.querySelector('.nav-wraper'), // Provide an element that will be the bounding container of the dropdown. This will make sure that the dropdown is aligned with the dropdown trigger
             constrainWidth: false,
-          });
-        }, []);
+          })
+        }, [])
+      
+      useEffect(() => {
+        const toggleHamburger = document.getElementsByClassName('hamburgerButton')[0]
+        const navBarLinks = document.getElementsByClassName('navbar-links')[0]
+        
+        //Handle click function that will toggle the navbarlinks to active 
+        const handleClick = () => {
+          navBarLinks.classList.toggle('active')
+        }
     
+        toggleHamburger.addEventListener('click', handleClick) // When the toggle hamburger btn is clicked, handle click fucntion will be executed
+        
+        // Now remove the event listener. this is not directly responsible for closing the nav bar but its generally good practice to 
+        // have a clean up function to clear the event listeners
+        return () => {
+          toggleHamburger.removeEventListener('click', handleClick)
+        }
+      }, [])
+
   return (
     <>
+
+    
     <ul id="dropdown1" class="dropdown-content navBarDropdown right-align">
           <li><a href="#!"><MdOutlineAccountBox/>{user.username}</a></li>
           <li>
@@ -28,21 +47,20 @@ const NavBar = () => {
             <MdLogout /> Logout
             </a>
           </li>
-          <li><Link to="/api/userProfiles/create"><MdCreate /> Create a Profile</Link></li>
-        
+          <li><Link to="/api/userProfiles/create"><MdCreate /> Create Profile</Link></li>
       </ul>
 
-   
 
-
+ 
       <nav className="nav-wraper mainNavBar">
               <Link to="/api/home"><img className="logo left" src={logo} alt="Logo"/></Link>
       
-              <a href="#" className="sidenav-trigger right hide-on-large-only" data-target="mobile-links">
-                  <i className="material-icons hide-on">menu</i>
+              <a className="hamburgerButton right" href="#!"  data-target="slide-out">
+                  <i className="material-icons">menu</i>
               </a>
 
-              <ul className="right hide-on-med-and-down">
+              <div className='navbar-links right'>
+              <ul>
                   <li><Link to="/api/home">Home</Link></li>
                   <li><Link to="/api/posts/create">Upload</Link></li>
                   <li><Link to="/api/posts">Posts</Link></li>
@@ -56,8 +74,27 @@ const NavBar = () => {
                     style={{ width: '2.5rem', height: 'auto' }}
                   /><i class="material-icons right">arrow_drop_down</i></a></li>
               </ul>
+              </div>
+           
     
       </nav>
+
+
+
+          {/* Here will be the side nav bar for smaller screens */}
+          {/* <ul className="sidenav right-align" id="slide-out" >
+            <li><a href="#!"><MdOutlineAccountBox/>{user.username}</a></li>
+            <li><Link to="/api/home">Home</Link></li>
+            <li><Link to="/api/posts/create">Upload</Link></li>
+            <li><Link to="/api/posts">Posts</Link></li>
+            <li><Link to="/api/userProfiles">Users</Link></li>
+            <li>
+              <a href="/api/login" onClick={logoutUser}>
+                <MdLogout /> Logout
+              </a>
+            </li>
+            <li><Link to="/api/userProfiles/create"><MdCreate /> Create Profile</Link></li>
+          </ul> */}
     </>
   )
 }
